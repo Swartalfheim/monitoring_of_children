85ca9dc23849290cee50734394dd386bebf69c9f
taghless <ghoulfik@gmail.com>
2025-05-13 11:46:59 +0200

    style

---
+++ monitoring/monitoring/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_page_view, name='home'),
+    path('about', views.about_page_view, name='about'),
+    path('login', views.login_page_view, name='login'),
+    path('register', views.register_page_view, name='register'),
+]
--- monitoring/monitoring/main/views.py
+++ monitoring/monitoring/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home_page_view(request):
+    return render(request, 'home.html')
+
+def about_page_view(request):
+    return render(request, 'about.html')
+
+def register_page_view(request):
+    return render(request, 'register.html')
+
+def login_page_view(requset):
+    return render(requset, 'login.html')

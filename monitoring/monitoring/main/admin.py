2c9d7b93d4d2a29c5c9f5c93ca28cf6db6cfbe5c
Denys <86122979+Swartalfheim@users.noreply.github.com>
2025-05-17 00:50:19 +0300

    add db

--- monitoring/monitoring/main/admin.py
+++ monitoring/monitoring/main/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import BooleanWidget
+from .models import Ditina
 
-# Register your models here.
+class DitinaResource(resources.ModelResource):
+    imya = fields.Field(attribute='imya', column_name="Ім'я")
+    pryimaie_gormony = fields.Field(
+        attribute='pryimaie_gormony',
+        column_name="Приймає гормони",
+        widget=BooleanWidget()
+    )
+
+    class Meta:
+        model = Ditina
+        fields = ('id', 'imya', 'prizvische', 'vik', 'zrist', 'region', 'pryimaie_gormony', 'data_stvorennia')
+        export_order = fields
+
+
+
+    def before_export(self, queryset, *args, **kwargs):
+        for obj in queryset:
+            obj.region = obj.get_region_display()
+
+@admin.register(Ditina)
+class DitinaAdmin(ImportExportModelAdmin):
+    resource_class = DitinaResource
+    list_display = ('id', 'imya', 'prizvische', 'vik', 'zrist', 'region', 'pryimaie_gormony')
--- monitoring/monitoring/monitoring/settings.py
+++ monitoring/monitoring/monitoring/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main.apps.MainConfig',
     'django.contrib.admin',
+    'admin_interface',
+    'colorfield',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
